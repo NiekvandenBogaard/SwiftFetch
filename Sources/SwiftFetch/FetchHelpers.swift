35f4af513ed97f46042ae9a79f38ba410e48c4b8
NiekBogaard <nvandenbogaard@move4mobile.com>
2020-03-12 16:42:01 +0100

    Fix visibility

--- Sources/SwiftFetch/FetchHelpers.swift
+++ Sources/SwiftFetch/FetchHelpers.swift
@@ -15,7 +15,7 @@ import Foundation
 ///   - query: The request query parameters.
 ///   - body: The request body.
 ///   - headers: The request headers.
-open func fetch(_ request: URLRequest, method: FetchMethod = .get, query: [String: String?]? = nil, body: FetchRequestBody? = nil, headers: [String: String]? = nil) -> FetchTask {
+public func fetch(_ request: URLRequest, method: FetchMethod = .get, query: [String: String?]? = nil, body: FetchRequestBody? = nil, headers: [String: String]? = nil) -> FetchTask {
     
     return Fetch.default.request(request, method: method, query: query, body: body, headers: headers)
 }
@@ -28,7 +28,7 @@ open func fetch(_ request: URLRequest, method: FetchMethod = .get, query: [Strin
 ///   - query: The request query parameters.
 ///   - body: The request body.
 ///   - headers: The request headers.
-open func fetch(_ url: URL, method: FetchMethod = .get, query: [String: String?]? = nil, body: FetchRequestBody? = nil, headers: [String: String]? = nil) -> FetchTask {
+public func fetch(_ url: URL, method: FetchMethod = .get, query: [String: String?]? = nil, body: FetchRequestBody? = nil, headers: [String: String]? = nil) -> FetchTask {
     
     return Fetch.default.request(url, method: method, query: query, body: body, headers: headers)
 }
